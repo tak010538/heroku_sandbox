2ff20093bd90b3285f0bac20e98f919a08c268fd
tak010538 <tak010538@gmail.com>
2023-10-03 16:47:12 +0900

    add login view

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -1,8 +1,9 @@
 # todoapp/urls.py
-
+from django.contrib.auth.views import LoginView
 from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('login/', LoginView.as_view(), name='login'),  # /login/ にログインページを設定
     path('todos/', views.todo_list, name='todo_list'),
 ]
