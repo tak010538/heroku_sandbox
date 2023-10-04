e4a81055e9621d80edf440ceeaae299b0ffde9ce
tak010538 <tak010538@gmail.com>
2023-10-04 11:32:02 +0900

    add todoitems fundction

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -4,6 +4,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login/', LoginView.as_view(), name='login'),  # /login/ にログインページを設定
+    path('login/', LoginView.as_view(
+        redirect_authenticated_user=True,
+        template_name='registration/login.html'
+    ), name='login'),  # /login/ にログインページを設定
     path('todos/', views.todo_list, name='todo_list'),
+    path('add-todo/', views.add_todo, name='add_todo'),  
 ]
--- todoapp/views.py
+++ todoapp/views.py
@@ -3,8 +3,18 @@
 from django.shortcuts import render, redirect
 from .models import TodoItem
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
 
 @login_required
 def todo_list(request):
     todos = TodoItem.objects.filter(user=request.user)
-    return render(request, 'todoapp/todo_list.html', {'todos': todos})
+    user_name = request.user.username  # ユーザ名を取得
+    return render(request, 'todoapp/todo_list.html', {'todos': todos, 'user_name': user_name})
+
+def add_todo(request):
+    if request.method == 'POST':
+        task = request.POST['task']
+        TodoItem.objects.create(user=request.user, task=task, completed=False)
+    return HttpResponseRedirect(reverse('todo_list'))
